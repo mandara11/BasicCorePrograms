e8c4be7b3604a6a484aa87549f17a15157d789da
mandara11 <mandaram2345@gmail.com>
2022-08-01 20:34:57 +0530

    ProjectName

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Programs");
+        }
+    }
+}
