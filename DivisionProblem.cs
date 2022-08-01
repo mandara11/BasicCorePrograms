6fd38e2e99f082e060795beb9ba64b23ad58d036
mandara11 <mandaram2345@gmail.com>
2022-08-01 21:30:04 +0530

    QuotientAndReminder

---
+++ DivisionProblem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class DivisionProblem
+    {
+        public static void CheckQutientAndRemainder()
+        {
+            int dividend = 50, divisor = 0;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Quotient :" + quotient);
+
+            Console.WriteLine("Remainder :" + remainder);
+        }
+    }
+}
