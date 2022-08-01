70423456b76b381845da057e5e7c458126bd3890
mandara11 <mandaram2345@gmail.com>
2022-08-01 21:24:36 +0530

    PrimeFactorization

---
+++ PrimeFactor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public static class PrimeFactor
+    {
+        public static void FindPrimeFactors()
+        {
+            Console.WriteLine("Enter a number: ");
+            int userInput = int.Parse(Console.ReadLine());
+            int i = 2;
+            while (userInput > 1)
+            {
+                Console.WriteLine(i);
+                userInput = userInput / i;
+            }
+            i++;
+        }
+    }
+}
