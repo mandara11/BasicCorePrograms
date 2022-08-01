e672c84afe2ea067c412e0ceb1a73b4e23511f88
mandara11 <mandaram2345@gmail.com>
2022-08-01 21:10:30 +0530

    HarmonicNumber

---
+++ Harmonic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class Harmonic
+    {
+        public static void Harmonicseries()
+        {
+            Console.WriteLine("Enter the n value of harmonic Series");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n != 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.Write("1/{0} + ", i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered Zero number and you need to enter Non Zero number");
+            }
+        }
+    }
+}
